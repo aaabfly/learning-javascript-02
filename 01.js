0f5a3f6796a6eb38ab2e78a5ec3b729fa9681eb5
0y4m4 <kd1274038@st.kobedenshi.ac.jp>
2019-06-28 09:49:30 +0900

    6/28 課題提出

--- 01.js
+++ 01.js
@@ -8,3 +8,19 @@ console.log("1");
 process.stdout.write("Fizz");
 process.stdout.write("Buzz");
 console.log();
+
+for (var i = 1; i <= 15; i++) {
+    if (i % 15 == 0) {
+        console.log("FizzBuzz");
+        continue;
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+        continue;
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+        continue;
+    } else {
+        console.log(i);
+        continue;
+    }
+}
